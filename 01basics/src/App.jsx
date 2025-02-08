c1ce6b87a63377dc8ca1046bb594688967fe6dab
Affan Shaikh <115262443+affanshaikh-dev@users.noreply.github.com>
2025-02-08 06:37:59 +0000

    project creation done by vite

---
+++ 01basics/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Chai Aur React with Vite | Shaikh Affan</h1>
+    </div>
+  );
+}
+
+export default App;
