ab2248b9758ff146dae8dbb51e8e62442dd2d41f
Affan Shaikh <115262443+affanshaikh-dev@users.noreply.github.com>
2025-02-25 11:33:20 +0000

    tailwind ongoing

---
+++ 03tailwindProps/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-red-300 text-black p-4 rounded-xl'>Tailwind Css</h1>
+    </>
+  )
+}
+
+export default App
