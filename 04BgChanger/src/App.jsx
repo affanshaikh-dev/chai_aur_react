f0bdf233b1c2b57e15fe125ca77b19f1a5f837b7
Affan Shaikh <115262443+affanshaikh-dev@users.noreply.github.com>
2025-03-01 13:41:26 +0000

    project 04 ongoing

---
+++ 04BgChanger/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+
+
+function App() {
+  const [color, setColor] = useState("olive")
+  return (
+    <>
+      <div className="w-full h-screen duration-200" style={{ backgroundColor: color }}>
+        <div className="fixed flex flex-wrap justify-center bottom-12 insert-x-0 px-2">Test</div>
+      </div>
+    </>
+  )
+}
+
+export default App
