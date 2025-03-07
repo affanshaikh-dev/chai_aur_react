12ea8404d5649ae4ee16932158be067c66eab773
Affan Shaikh <115262443+affanshaikh-dev@users.noreply.github.com>
2025-03-07 07:32:43 +0000

    currecny convertor Ongoing

---
+++ 05CurrencyConvertor/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { InputBox } from './components'
+import {useCurrencyInfo} from './hooks/useCurrencyInfo'
+
+
+function App() {
+  const [amount, setAmount] = useState(0)
+
+  return (
+    <>
+      <h1></h1>
+    </>
+  )
+}
+
+export default App
