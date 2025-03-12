db5fde679d246d92778a1b65cac259d5735c9e7d
Affan Shaikh <115262443+affanshaikh-dev@users.noreply.github.com>
2025-03-12 10:14:55 +0530

    Update App.jsx

--- 06ReactRouter/src/App.jsx
+++ 06ReactRouter/src/App.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
+import About from "./components/About/About";
 
 function App() {
     return (
-        <></>
-    )
+        <Router>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route path="about" element={<About />} />
+                    {/* Add other routes here */}
+                </Route>
+            </Routes>
+        </Router>
+    );
 }
 
-export default App
+export default App;
