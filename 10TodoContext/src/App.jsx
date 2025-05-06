07e847c2d8d4cfe8235e2bca035afcd240002183
Affan Shaikh <115262443+affanshaikh-dev@users.noreply.github.com>
2025-05-06 10:45:39 +0000

    local storage + Context Api

--- 10TodoContext/src/App.jsx
+++ 10TodoContext/src/App.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react'
 import { TodoProvider } from './contexts'
+import TodoFrom from './components/TodoForm'
+import TodoItem from './components/TodoItem'
 import './App.css'
 
 function App() {
@@ -18,7 +20,7 @@ function App() {
   }
 
   const toggleComplete = (id) => {
-    setTodos((prev) => prev.map((prevTodo) => prevTodo === id ? { ...prev, completed: !prevTodo.completed } : prevTodo))
+    setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? { ...prevTodo, completed: !prevTodo.completed } : prevTodo))
   }
 
   useEffect(() => {
@@ -37,11 +39,17 @@ function App() {
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
-          <div className="mb-4">
+          <div className="mb-4">  
             {/* Todo form goes here */}
+            <TodoFrom />
           </div>
           <div className="flex flex-wrap gap-y-3">
             {/*Loop and Add TodoItem here */}
+            {todos.map((todo) => (
+              <div key={todo.id} className="w-full">
+                <TodoItem todo={todo} />
+              </div>
+            ))}
           </div>
         </div>
       </div>
--- 10TodoContext/src/components/TodoItem.jsx
+++ 10TodoContext/src/components/TodoItem.jsx
@@ -1,9 +1,19 @@
 import React, { useState } from 'react'
-import { useTodo } from '../contexts';
+import { useTodo } from '../contexts/TodoContext'; 
 
 function TodoItem({ todo }) {
     const [isTodoEditable, setIsTodoEditable] = useState(false)
+    const [todoMsg, setTodoMsg] = useState(todo.todo)
     const {updateTodo, deleteTodo, toggleComplete} = useTodo()
+
+    const editTodo = () => {
+        updateTodo(todo.id, {...todo, todo: todoMsg })
+        setIsTodoEditable(false)
+    }
+
+    const toggleCompleted = () => {
+        toggleComplete(todo.id)
+    }
 
     return (
         <div
@@ -15,7 +25,7 @@ function TodoItem({ todo }) {
                 type="checkbox"
                 className="cursor-pointer"
                 checked={todo.completed}
-                onChange={toggleComplete}
+                onChange={toggleCompleted}
             />
             <input
                 type="text"
