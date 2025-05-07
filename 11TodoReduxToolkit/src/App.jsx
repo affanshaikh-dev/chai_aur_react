c31d55c6cb738fd4189268f9981e91a3e784ab8a
Affan Shaikh <115262443+affanshaikh-dev@users.noreply.github.com>
2025-05-07 08:52:53 +0000

    redux-toolkit Done

---
+++ 11TodoReduxToolkit/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import AddTodo from './Components/AddTodo'
+import Todos from './Components/Todos'
+
+function App() {
+
+  return (
+    <>
+      <h1>Redux Toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
